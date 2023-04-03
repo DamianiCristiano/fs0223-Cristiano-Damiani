a2d68a38898ec0906cc5d9e7e0e9461eccb60cb0
DamianiCristiano <damiani.cristiano96@gmail.com>
2023-04-03 17:03:37 +0200

    test g1-s2

---
+++ m1/s2/g1/js/index.js
@@ -0,0 +1,36 @@
+document.getElementById('alu').innerHTML = ('')
+
+var x = prompt('come sta tra 0/100?');
+
+console.log(x);
+
+if 
+(x >= 33 && x <= 66) 
+{document.write ('mhe')}
+else if
+(x == 22)
+{document.write ('sei un king')}
+else if
+(x == 4)
+{document.write ('sei un jhin')}
+else if  
+(x > 10 && x < 33)
+{document.write ('male')}
+else if
+(x > 66)
+{document.write ('bene')}
+else if
+(x <= 10)
+{document.write ('na merda')}
+
+
+/*var time = true/false
+if
+(time = true)
+{document.write('accendi luci')}
+else
+{document.write('spegni luci')}*/
+
+
+
+
---
+++ m1/s2/g1/js/naming.js
@@ -0,0 +1,4 @@
+/* test a caso x = 122
+    y = 3
+
+console.log(x + y) */
