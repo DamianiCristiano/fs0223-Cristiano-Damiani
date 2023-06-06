d82029005f105a6870017950979ce85a74b007bc
DamianiCristiano <damiani.cristiano96@gmail.com>
2023-06-06 15:17:45 +0200

    w3 s3 g2

---
+++ m3/s3/g2/p1/src/app/driven/driven.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driven',
+  templateUrl: './driven.component.html',
+  styleUrls: ['./driven.component.scss']
+})
+export class DrivenComponent {
+
+  hero1 = {
+    name: '',
+    alterEgo: '',
+    power: '',
+    enemy: '',
+    planet: '',
+    weakness: ''
+  };
+
+  hero2 = {
+    name: '',
+    alterEgo: '',
+    power: '',
+    enemy: '',
+    planet: '',
+    weakness: ''
+  };
+
+  submitForm(form: any) {
+    if (form.valid) {
+      console.log('Dati inviati:', form.value);
+      // Aggiungi qui la logica per salvare il supereroe
+    }
+  }
+}
+
